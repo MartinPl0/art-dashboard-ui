f0e5a50fa643029b0889bd22985d88d32ef6828b
MartinPl0 <mathas000@gmail.com>
2023-08-10 17:14:07 +0200

    Changes to privatePage.js

--- pages/private/privatePage.js
+++ pages/private/privatePage.js
@@ -1,22 +1,24 @@
 import { makeApiCall } from '../../components/api_calls/api_calls';
-import Head from 'next/head'; 
 
 export async function getServerSideProps(context) {
     const { req, res } = context;
 
     // Call the API endpoint using 'makeApiCall'
     const result = await makeApiCall('/api/v1/check_auth', 'GET', {}, {}, {}, req);
+    const actualPath = req.url.replace(/\/_next\/data\/[^/]+/, '');
 
     // If the user is not authenticated or the request failed, redirect to the login page
-    if (result.detail === 'Not authenticated' || result.detail === 'Request failed') {
+    if (result.detail !== 'Authenticated') {
+        console.log('Redirecting to login page due to:', result.detail);
         return {
             redirect: {
-                destination: `/?loginRequired=true&next=${req.url}`,
+                destination: `/?loginRequired=true&next=${actualPath}`,
                 permanent: false
             }
         };
     }
 
+    console.log('User authenticated, rendering Private Page.');
     // If the user is authenticated, render the page
     return {
         props: {}, // Will be passed to the page component as props
@@ -26,10 +28,6 @@ export async function getServerSideProps(context) {
 function PrivatePageComponent() {
     return (
         <div>
-            <Head>
-                <title>Private Page</title>
-                <link rel="icon" href="/redhat-logo.png"/>
-            </Head>
             <h1>Private Page</h1>
             <p>Only accessible to logged in ART members</p>
         </div>
